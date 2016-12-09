e1af7c9e936dea12bae74779b3793196e664b11a
Alexander <alexander.tsukanovvv@gmail.com>
2016-12-09 19:52:11 +0200

    Task 5 fixes

--- src/main/java/oracleacademy/tsukanov/homework/task5/Translator.java
+++ src/main/java/oracleacademy/tsukanov/homework/task5/Translator.java
@@ -8,26 +8,36 @@ import java.util.regex.Pattern;
 
 public class Translator {
 
-    private String translate(String translatorPath, String translatableFilePath) throws FileNotFoundException, UnsupportedEncodingException {
-        Pattern p = Pattern.compile("\\w+");
+    private String translate(String translatorPath, String translatableFilePath, String charset) throws FileNotFoundException, UnsupportedEncodingException {
+        Pattern p = Pattern.compile("([A-Za-zА-Яа-я0-9ё]+)([\\W]+|$)");
         HashMap<String, String> translation = new HashMap<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(translatorPath), "windows-1251"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(translatorPath), charset));
         StringBuilder sb = new StringBuilder();
         String word;
         try {
             while ((word = br.readLine()) != null) {
-                String[] words = word.split(";-;");
-                translation.put(words[0], words[1]);
+                String[] words = word.split("[^a-zA-Zа-яА-Я0-9ё]+");
+                for (int i = 0; i < words.length; i++) {
+                    translation.put(words[0], words[1]);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        br = new BufferedReader(new FileReader(translatableFilePath));
+        br = new BufferedReader(new InputStreamReader(new FileInputStream(translatableFilePath), charset));
         try {
             while ((word = br.readLine()) != null) {
                 Matcher m = p.matcher(word);
                 while (m.find()) {
-                    sb.append(translation.get(m.group().toLowerCase())).append(" ");
+                    System.out.println(m.group());
+                    if (m.group(1).matches("[A-Z][a-z]+")) {
+                        String upperCase = translation.get(m.group(1).toLowerCase());
+                        upperCase = Character.toUpperCase(upperCase.charAt(0)) + upperCase.substring(1);
+                        sb.append(upperCase);
+                    } else {
+                        sb.append(translation.get(m.group(1).toLowerCase()));
+                    }
+                    sb.append(m.group(2));
                 }
             }
         } catch (IOException e) {
@@ -36,17 +46,17 @@ public class Translator {
         return sb.toString();
     }
 
-    void translatorInit(String translatorPath, String translatableFilePath) throws IOException {
+    void translatorInit(String translatorPath, String translatableFilePath, String charset) throws IOException {
         File directory;
         int action;
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to Translator!");
         menu:
         do {
-            System.out.println("\n"+
-            "Please, choose the action:\n"+
-            "1. Select dictionary.\n" +
-            "2. Exit.\n");
+            System.out.println("\n" +
+                    "Please, choose the action:\n" +
+                    "1. Select dictionary.\n" +
+                    "2. Exit.\n");
             action = in.nextInt();
             switch (action) {
                 case 1:
@@ -63,12 +73,12 @@ public class Translator {
                         System.out.println(i + 1 + ". " + listOfFiles[i].getName().replaceAll("\\.\\w+", ""));
                     }
                     action = in.nextInt();
-                    String result =  translate(listOfFiles[action - 1].getAbsolutePath(), translatableFilePath);
+                    String result = translate(listOfFiles[action - 1].getAbsolutePath(), translatableFilePath, charset);
                     System.out.println();
                     System.out.println(result);
                     continue menu;
                 case 2:
-                    System.out.println("Bye");
+                    System.out.println("Bye, bye!");
                     System.exit(0);
                     break;
                 default:
